4e9bc6873ce516534e6bea9ed8df0711f4742f91
Thami <thami.sadouk@gmail.com>
2022-01-13 07:04:05 +0100

    created hp, style globals, new Quizz view file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to the quizz!"/>
+  <div class="home text-center">
+    <h1>CineMojo</h1>
+    <div class="board">
+      <p>Welcome to the quizz ! <br>
+        You'll be asked a series of "Yes or No" questions. <br>
+        Answer as many as you can in the allowed time ! <br>
+        Good luck !
+      </p>
+      <router-link to="/quizz" tag="button" class="btn primary">Start</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
---
+++ src/views/Quizz.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="quizz">
+    <h1>This is the quizz page</h1>
+  </div>
+</template>
